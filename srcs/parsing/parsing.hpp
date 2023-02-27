9db85d4c159b706e7cfb006e1cfcb8c66f61e92f
rmney <rmneyz@gmail.com>
2023-02-28 00:26:07 +0100

    construting server

--- srcs/parsing/parsing.hpp
+++ srcs/parsing/parsing.hpp
@@ -6,7 +6,7 @@
 /*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 21:53:42 by rimney            #+#    #+#             */
-/*   Updated: 2023/02/27 06:34:17 by rimney           ###   ########.fr       */
+/*   Updated: 2023/02/28 00:25:45 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,6 @@
 #include <sstream>
 #include <vector>
 #include <map>
-
-class serverParsing
-{
-    private :
-        std::string key; // the line that includes the '{' bracket and must implemet a way how to find the other  one
-        std::vector<std::string> *elems; // elements after finding the '{' and untill the '}' bracket
-    public : 
-        
-};
 
 class server_location
 {
@@ -43,92 +34,28 @@ class server_location
         std::string cgiPath; // location of the interpreter 
         std::string cgiExt; // cgi extension
     public :
-        server_location()
-        {
-        }
-        void        setName(std::string name)
-        {
-            this->name = name;
-        }
-        std::string getName(void)
-        {
-            return (this->name);
-        }
-        void    setRoot(std::string root)
-        {
-            this->root = root;
-        }
-        std::string getRoot(void)
-        {
-            return (this->root);
-        }
-        bool    getAutoIndex(void)
-        {
-            return (this->autoIndex);
-        }
-        void    setAutoIndex(bool autoIndex)
-        {
-            this->autoIndex = autoIndex;
-        }
-        void        setAllowMethods(std::string *allowMethods)
-        {
-            this->allowMethods = allowMethods;
-        }
-        std::string        *getAllocMethods(void)
-        {
-            return (this->allowMethods);
-        }
-        void    setIndex(std::string index)
-        {
-            this->index = index;
-        }
-        std::string getIndex(void)
-        {
-            return (this->index);
-        }
-        std::string    getRedirection(void)
-        {
-            return (this->rediection);
-        }
-        void    setRedirection(std::string redirection)
-        {
-            this->rediection = redirection;
-        }
-        void    setAlias(std::string alias)
-        {
-            this->alias = alias;
-        }
-        std::string getAlias(void)
-        {
-            return (this->alias);
-        }
-        void    setIsCgi(bool is_cgi)
-        {
-            this->is_cgi = is_cgi;
-        }
-        bool getIsCgi(void)
-        {
-            return (this->is_cgi);
-        }
-        void    setCgiPath(std::string cgiPath)
-        {
-            this->cgiPath = cgiPath;
-        }
-        std::string    getCgiPath(void)
-        {
-            return (this->cgiPath);
-        }
-        void    setCgiExt(std::string cgiExt)
-        {
-            this->cgiExt = cgiExt;
-        }
-        std::string getCgiExt(void)
-        {
-            return (this->cgiExt);
-        }
-        ~server_location()
-        {
-        }
+        server_location(){}
+        void        setName(std::string name){this->name = name;}
+        std::string getName(void){return (this->name);}
+        void    setRoot(std::string root){this->root = root;}
+        std::string getRoot(void){return (this->root);}
+        bool    getAutoIndex(void){return (this->autoIndex);}
+        void    setAutoIndex(bool autoIndex){this->autoIndex = autoIndex;}
+        void        setAllowMethods(std::string *allowMethods){this->allowMethods = allowMethods;}
+        std::string        *getAllocMethods(void){return (this->allowMethods);}
+        void    setIndex(std::string index){this->index = index;}
+        std::string getIndex(void){return (this->index);}
+        std::string    getRedirection(void){return (this->rediection);}
+        void    setRedirection(std::string redirection){this->rediection = redirection;}
+        void    setAlias(std::string alias){this->alias = alias;}
+        std::string getAlias(void){return (this->alias);}
+        void    setIsCgi(bool is_cgi){this->is_cgi = is_cgi;}
+        bool getIsCgi(void){return (this->is_cgi);}
+        void    setCgiPath(std::string cgiPath){this->cgiPath = cgiPath;}
+        std::string    getCgiPath(void){return (this->cgiPath);}
+        void    setCgiExt(std::string cgiExt){this->cgiExt = cgiExt;}
+        std::string getCgiExt(void){return (this->cgiExt);}
+        ~server_location(){}
         
 };
 
@@ -153,47 +80,18 @@ class server_parser : public server_location
             for(size_t i = 0; i < serverVec.size(); i++)
                 std::cout << serverVec[i] << '\n';
 
-            std::cout << "/////////////////// server " << this->server_index << "//////////////\n\n";
-            
+            std::cout << "/////////////////// server " << this->server_index << "//////////////\n\n"; 
         }
-        void    setServerIndex(int index)
-        {
-            this->server_index = index;
-        }
+        void    setServerIndex(int index){ this->server_index = index;}
         ~server_parser() {};
-        int getPort(void)
-        {
-            return (this->port);
-        }
-        int getHost(void)
-        {
-            return (this->host);
-        }
-        std::string getServerName(void)
-        {
-            return (this->server_name);
-        }
-        std::string getErrorPage(void)
-        {
-            return (this->error_page);
-        }
-        int getCmds(void)
-        {
-            return (this->cmds);
-        }
-        std::string getRoot(void)
-        {
-            return (this->root);
-        }
-        std::string getIndex(void)
-        {
-            return (this->index);
-        }
-        server_location *getLocations(void)
-        {
-            return (this->location);
-        }
-        
+        int getPort(void){ return (this->port);}
+        int getHost(void){ return (this->host);}
+        std::string getServerName(void){ return (this->server_name);}
+        std::string getErrorPage(void){ return (this->error_page);}
+        int getCmds(void){ return (this->cmds);}
+        std::string getRoot(void){return (this->root);}
+        std::string getIndex(void){return (this->index);}
+        server_location *getLocations(void){return (this->location);}
 };
 
 class config_parser : public server_parser
@@ -234,16 +132,14 @@ class config_parser : public server_parser
                         {
                             while(tempConf[i] != "}")
                                 i++;
-                            
                         }
                         i++;
                     }
                     closing_bracket = i;
-                    servers[server_index].construct_server(tempConf.begin() + opening_bracket, tempConf.begin() + closing_bracket + 1);
+                    servers[server_index].construct_server(tempConf.begin() + opening_bracket, tempConf.begin() + closing_bracket + 1); // constructing server
                     server_index += 1;
                 }
             }
-            // exit(0);
         }
         void    servers_index_init()
         {
@@ -260,10 +156,7 @@ class config_parser : public server_parser
                 }
                 return (count);
             }
-        ~config_parser()
-        {
-            delete [] this->servers;
-        }
+        ~config_parser(){delete [] this->servers;}
         };
 
 #endif
