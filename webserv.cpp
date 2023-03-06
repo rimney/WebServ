3dec6f55518c6df3d2b530d2ce0fa0d392984cbb
rmney <rmneyz@gmail.com>
2023-03-06 23:52:51 +0100

    done

--- webserv.cpp
+++ webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 20:56:08 by eel-ghan          #+#    #+#             */
-/*   Updated: 2023/03/06 23:36:54 by rimney           ###   ########.fr       */
+/*   Updated: 2023/03/06 23:50:28 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,12 @@
 
 int main(int argc, char **argv)
 {
-    (void)argc;
     if(argc == 2)
     {
         config_parser servers(argv[1]);
         // config_parser aa = servers;
-        // std::cout << servers;
-        system("leaks webserv");
+        std::cout << servers;
+        // system("leaks webserv");
 
     }
 }
