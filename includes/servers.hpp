6edaa03f118ef0a664ca80f30c837be0842845c3
Elbarae El-ghannouchi <barae.gh@gmail.com>
2023-03-07 01:12:38 +0100

    servers class

---
+++ includes/servers.hpp
@@ -0,0 +1,40 @@
+
+
+#ifndef SERVERS_HPP
+# define SERVERS_HPP
+
+
+
+# include <sys/select.h>
+# include <map>
+# include "webserv.hpp"
+// # include "../parsing/parsing.hpp"
+# include "server.hpp"
+
+class servers
+{
+    private:
+        fd_set                  _set_fds;
+        fd_set                  _set_read_fds;
+        // fd_set                  _set_write_fds;
+        // config_parser           _config;
+        int                     _servers_count;
+        int                     _max_fd;
+        std::vector<server>     _servers;
+        std::map<int, server>   _fds_cnx;
+
+    public:
+        servers();
+        // servers(config_parser config);
+        servers(servers const & s);
+        ~servers();
+
+        servers &   operator=(servers const & s);
+
+        void    run();
+
+    private:
+        void    setup();
+};
+
+#endif
