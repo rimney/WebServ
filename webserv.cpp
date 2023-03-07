92f08de84548c4e7dd105a63bbe414ec7ef16c66
Elbarae El-ghannouchi <barae.gh@gmail.com>
2023-03-07 01:12:04 +0100

    the main

--- webserv.cpp
+++ webserv.cpp
@@ -6,7 +6,7 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 20:56:08 by eel-ghan          #+#    #+#             */
-/*   Updated: 2023/02/25 21:00:22 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2023/03/07 01:00:26 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,30 @@
 
 int main()
 {
+    try
+    {
+        servers     s;
+
+        s.run();
+        
+        // server s(440, INADDR_ANY);
+        // server s1(880, INADDR_ANY);
+
+        // while (1)
+        // {
+        //     s.accept();
+        //     s1.accept();
+        //     // s.receive();
+        //     std::cout << s.get_request() << '\n';
+        //     close(s.get_fd_connection());
+        //     close(s1.get_fd_connection());
+        // }
+    
+    }
+    catch(const std::string& msg)
+    {
+        std::cerr << msg << '\n';
+    }
     
     return 0;
 }
