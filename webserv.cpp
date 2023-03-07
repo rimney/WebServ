c7932e4a94f69a5e4f04e6cb9f9703c49c2e2d9c
Elbarae El-ghannouchi <barae.gh@gmail.com>
2023-03-07 01:22:07 +0100

    main

--- webserv.cpp
+++ webserv.cpp
@@ -6,38 +6,30 @@
 /*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 20:56:08 by eel-ghan          #+#    #+#             */
-/*   Updated: 2023/03/07 01:00:26 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2023/03/07 01:19:09 by eel-ghan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/webserv.hpp"
+#include "includes/parsing.hpp"
+#include "includes/servers.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    try
+    if(argc == 2)
     {
-        servers     s;
+        try
+        {
+            servers     s;
 
-        s.run();
-        
-        // server s(440, INADDR_ANY);
-        // server s1(880, INADDR_ANY);
-
-        // while (1)
-        // {
-        //     s.accept();
-        //     s1.accept();
-        //     // s.receive();
-        //     std::cout << s.get_request() << '\n';
-        //     close(s.get_fd_connection());
-        //     close(s1.get_fd_connection());
-        // }
-    
+            s.run();
+        }
+        catch(const std::string& msg)
+        {
+            std::cerr << msg << '\n';
+        }
     }
-    catch(const std::string& msg)
-    {
-        std::cerr << msg << '\n';
-    }
+    else
+        std::cout << "bad args\n";
     
     return 0;
 }
