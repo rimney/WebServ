447286bb155dd24812353f2c70ab4380255623c9
rmney <rmneyz@gmail.com>
2023-03-08 23:30:45 +0100

    merging

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 NAME = webserv
 
-SERVER = srcs/server
+SRC = webserv.cpp srcs/parsing/Parsing.cpp
 
-SRC = webserv.cpp ${SERVER}/server.cpp ${SERVER}/servers.cpp srcs/parsing/parsing.cpp
-
-CFLAGS = -Wall -Wextra -Werror -std=c++98 #-fsanitize=address -g3
+CFLAGS = -Wall -Wextra -Werror -std=c++98 -fsanitize=address -g3
 
 ${NAME} : ${SRC}
 		@g++ ${CFLAGS} ${SRC} -o ${NAME}
--- webserv.cpp
+++ webserv.cpp
@@ -3,44 +3,50 @@
 /*                                                        :::      ::::::::   */
 /*   webserv.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/25 20:56:08 by eel-ghan          #+#    #+#             */
-/*   Updated: 2023/03/07 23:34:55 by eel-ghan         ###   ########.fr       */
+/*   Updated: 2023/03/08 16:19:01 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "includes/webserv.hpp"
 #include "includes/parsing.hpp"
-#include "includes/servers.hpp"
+
+
 
 int main(int argc, char **argv)
 {
-
     if(argc == 2)
     {
-        try
+        config_parser servers(argv[1]);
+        // server_parser *p = servers.getServersObject();
+        // config_parser aa = servers;
+        std::cout << servers;
+        int listenn = socket(AF_INET, SOCK_STREAM, 0);
+        if(listen == -1)
         {
-            config_parser config(argv[1]);
-            
-            // std::cout << config;
-
-            servers     s(config);
-            if (s.setup(config.getServersObject()))
-            {
-                // system("leaks webserv");
-                return 1;
-            }
-            s.run();
-
-            // system("leaks webserv");
+            std::cerr << "Error cannot create Socket !\n";
+            exit(0);
         }
-        catch(const std::string& msg)
+        sockaddr_in addr;
+        addr.sin_family = AF_INET;
+        addr.sin_port = htons(8080);
+        inet_pton(AF_INET, "127.0.0.1", &addr);
+        if (bind(listenn, (sockaddr *)&addr, sizeof(addr)) == -1)
         {
-            std::cerr << msg << '\n';
+            std::cerr << "Error Cannot Bind To IP/PORT";
+            exit(0);
         }
+        if(listen(listenn, SOMAXCONN) == -1)
+        {
+            std::cerr << "Can't listen ";
+            exit(0);
+        }
+        
+        
+        // std::cout << p[0].getPortObject();
+        // system("leaks websrv");
+
     }
-    else
-        std::cout << "ERROR: bad args number\n";
-    
-    return 0;
 }
