18a7fb93bad398125c41ecd6f4e5c5cd12d22f2e
rmney <rmneyz@gmail.com>
2023-03-09 07:07:37 +0100

    server set up is done (rimney)

--- srcs/server/rserver.cpp
+++ srcs/server/rserver.cpp
@@ -6,7 +6,7 @@
 /*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 04:16:59 by rimney            #+#    #+#             */
-/*   Updated: 2023/03/09 05:07:07 by rimney           ###   ########.fr       */
+/*   Updated: 2023/03/09 07:06:19 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,43 @@
 #include <unistd.h>
 #include <string.h>
 #include <netdb.h>
+#include <fstream>
+#include <sstream>
+
+void handleConnection(int clientSocket)
+{
+    char buf[4096];
+    while (true) 
+    {
+        memset(buf, 0, 4096);
+        int received = recv(clientSocket, buf, 4096, 0);
+        if (received == -1)
+        {
+            std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
+            exit(1);
+        }
+        else if (received == 0)
+        {
+            std::cout << "The client disconnected." << std::endl;
+            break;
+        } 
+        else
+        {
+            std::ifstream file("index.html");
+            if (!file)
+            {
+                std::cerr << "Can't open index.html\n";
+                exit(1);
+            }
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            std::string contents(buffer.str()); 
+            std::string response = "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(contents.size()) + "\r\n\r\n" + contents;
+            send(clientSocket, response.c_str(), response.size(), 0);
+        }
+    }
+    close(clientSocket);
+}
 
 int main()
 {
@@ -58,6 +95,7 @@ int main()
     memset(host, 0, NI_MAXHOST);
     memset(svc, 0, NI_MAXSERV);
 
+    std::cout << "Waiting for Connection ...\n";
     int result = getnameinfo((sockaddr *)&client, sizeof(client), host, NI_MAXHOST, svc, NI_MAXSERV, 0);
     if (result)
     {
@@ -66,24 +104,38 @@ int main()
     else
     {
         inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
-        std::cout << host << " connected on " << htons(client.sin_port) << '\n';
+        std::cout << host << " connected on " << ntohs(client.sin_port) << '\n';
     }
     char buf[4096];
-    while (true) {
-    memset(buf, 0, 4096);
-    int received = recv(clientSocket, buf, 4096, 0);
-    if (received == -1) {
-        std::cerr << "Error receiving data: " << strerror(errno) << std::endl;
-        exit(1);
-    } else if (received == 0) {
-        std::cout << "The client disconnected." << std::endl;
-        break;
-    } else {
-        std::string request = buf;
-        std::string response = "HTTP/1.1 200 OK\r\nContent-Length: 12\r\n\r\nHello world!";
-        send(clientSocket, response.c_str(), response.size(), 0);
-    }
-}
-    close(clientSocket);
     
+    while (1)
+    {
+        memset(buf, 0, 4096);
+        int received = recv(clientSocket, buf, 4096, 0);
+        if (received == -1)
+        {
+            std::cerr << "Error receiving data: "  << std::endl;
+            exit(1);
+        }
+        else if (received == 0)
+        {
+            std::cout << "The client disconnected." << std::endl;
+            break;
+        } 
+        else {
+            std::cout << buf;
+            std::ifstream file("index.html");
+            if (!file)
+            {
+                std::cerr << "Can't open index.html\n";
+                exit(1);
+            }
+            std::stringstream buffer;
+            buffer << file.rdbuf();
+            std::string contents(buffer.str());
+            std::string response = "HTTP/1.1 200 OK\r\nContent-Length: " + std::to_string(contents.size()) + "\r\n\r\n" + contents;
+            send(clientSocket, response.c_str(), response.size(), 0);
+            }
+    }
+    close(clientSocket);
 }
