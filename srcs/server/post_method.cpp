5634267d9818a34500af4b5499316a63b73b672f
mohammed <togachivon@gmail.com>
2023-03-18 01:12:30 +0100

    multipart

--- srcs/server/post_method.cpp
+++ srcs/server/post_method.cpp
@@ -4,50 +4,32 @@
 size_t remove_header(std::string &request, size_t i,std::string &buffer,std::string &filename,std::string &upload)
 {
     int count_qouet = 0;
-    bool boundry = false;
-     
-    for(size_t j = i; j < (size_t)request.length();j++)
+    if(i >= request.length() || !filename.empty())
     {
-        std::cerr << request[j] << std::endl;
-        if(request[j] != '-' &&  request[j] < 48 && request[j] > 57)
-            break;
-        else if(request[j + 1] == 13 && request[j + 2] == 10)
-        {
-            i = j + 3;
-            boundry = true;
-            break;
-        }
+        std::ofstream post(upload + filename);
+        post << buffer;
+        post.close();
+        filename.clear();
+        buffer.clear();
     }
-    if(boundry == true)
+    else
     {
-        if(i >= request.length() || !filename.empty())
+        for(; i < (size_t)request.length();i++)
         {
-            std::ofstream post(upload + filename);
-            buffer.erase(buffer.length() - 2 , 2);
-            post << buffer;
-            post.close();
-            filename.clear();
-            buffer.clear();
-        }
-        else
-        {
-            for(; i < (size_t)request.length();i++)
+            if(request[i] == 13 && request[i + 1] == 10 
+                && request[i + 2] == 13 && request[i + 3] == 10)
             {
-                if(request[i] == 13 && request[i + 1] == 10 
-                    && request[i + 2] == 13 && request[i + 3] == 10)
-                {
-                        i += 4;
-                        break;
-                }
-                if(count_qouet == 3)
-                {   
-                    for(;i < (size_t)request.length() && request[i] != '"' ;i++)
-                        filename += request[i];
-                    count_qouet = 0;
-                }
-                else if(request[i] == '"')
-                    count_qouet++;
+                    i += 4;
+                    break;
             }
+            if(count_qouet == 3)
+            {   
+                for(;i < (size_t)request.length() && request[i] != '"' ;i++)
+                    filename += request[i];
+                count_qouet = 0;
+            }
+            else if(request[i] == '"')
+                count_qouet++;
         }
     }
     return i ;
@@ -57,11 +39,13 @@ void    server::post_method(server_parser &serv)
 {
     std::string extention;
     std::string boundary;
+    long boundary_pos = 0;
     std::string filename;
     std::string buffer;
+    bool is_boundary = false;
     (void)serv;
     std::string upload = "/Users/brmohamm/Desktop/WebServ/upload/";
-    std::cout << request.get_body() << std::endl;
+    // std::cout << request.get_body() << std::endl;
     if(!request.get_body().empty())
     {
         //!serv.getServerLocationsObject()[request.get_start_line().location_index].getLocationNameObject().empty()
@@ -71,19 +55,32 @@ void    server::post_method(server_parser &serv)
             {
                 if(strncmp(request.get_header().find("Content-Type")->second.c_str(),"multipart",9) == 0)//multipart
                 {
-                    for(size_t i = request.get_header().find("Content-Type")->second.find("=") + 1; i < (size_t)request.get_header().find("Content-Type")->second.length();i++)
+                    for(size_t i = request.get_header().find("Content-Type")->second.find("=") + 1; i < (size_t)request.get_header().find("Content-Type")->second.length();i++)//find boundary
                     {
                         if (request.get_header().find("Content-Type")->second[i] != '-')
                                boundary +=  request.get_header().find("Content-Type")->second[i];
                     }
-                    // std::cout << ">> " <<request.get_body()[request.get_body().find(boundary,request.get_body().find(boundary) + 1) - 32]  << std::endl;
-                    // for (size_t i = 0 ; i < (size_t)request.get_body().length();i++)
-                    // {
-                    //     if(request.get_body()[i] == '-')
-                    //         i = remove_header(request.get_body(),i,buffer,filename,upload);
-                    //     if(i < (size_t)request.get_body().length())
-                    //         buffer += request.get_body()[i];
-                    // }
+                    for (size_t i = 0 ; i < (size_t)request.get_body().length();i++)
+                    {
+                        boundary_pos = request.get_body().find(boundary,request.get_body().find(boundary) + boundary_pos);
+                        i = remove_header(request.get_body(),i,buffer,filename,upload);
+                        for(; i < (size_t)request.get_body().length();i++)
+                        {
+                            if((request.get_body()[i] == 13 && request.get_body()[i + 1] == 10 && request.get_body()[i + 2] == '-') 
+                                || (request.get_body()[i ] == 10 && request.get_body()[i + 1] == '-') 
+                                || (request.get_body()[i ] == 13 && request.get_body()[i + 1] == '-'))
+                            {
+                                if(request.get_body().find(boundary,request.get_body().find(boundary) + i) < i + 32)
+                                {
+                                    break;
+                                    is_boundary = true;
+                                }
+                                is_boundary = false;
+                            }  
+                            if(is_boundary == false)
+                                buffer += request.get_body()[i];
+                        }
+                    }
                 }
                 else
                 {
