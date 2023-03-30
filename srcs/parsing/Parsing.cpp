369c3381f08126c2ae7d6eaae30761f4a7ac6f3e
rimney <rmneyz@gmail.com>
2023-03-30 04:14:03 +0000

    cgi

--- srcs/parsing/Parsing.cpp
+++ srcs/parsing/Parsing.cpp
@@ -6,7 +6,7 @@
 /*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/07 03:50:36 by rimney            #+#    #+#             */
-/*   Updated: 2023/03/30 04:04:57 by rimney           ###   ########.fr       */
+/*   Updated: 2023/03/30 04:08:18 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -271,8 +271,6 @@ void    server_location::getUpload(std::string *Keys, size_t size)
     this->upload = Keys[size - 1];
     delete [] Keys; 
 }
-
-
 
 void    server_location::checkCgiAllowed(void)
 {
@@ -741,6 +739,7 @@ void    server_parser::construct_server(std::vector<std::string>::iterator first
             getServerName(stringSplit(serverVec[i], ' ', &temp_size), temp_size);
             if(this->server_names.size() == 0)
                 this->server_names.push_back("localhost ");
+            std::cout << getServerNamesObject()
         }
         else if(!strncmp(serverVec[i].c_str(), "error", 5))
         {
